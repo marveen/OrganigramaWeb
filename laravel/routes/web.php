f2ee36d6ce98ea77e914abfe9a53ee8295a9afb6
Rodrigo Valenzuela <rvalenzuela@vgroup.cl>
2018-11-27 22:51:35 -0300

    commited funcionando local

---
+++ laravel/routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::resource('ContactoWeb', 'ContactoWebController');
+
+
+
+
+
+Route::get('hello', function () {
+    return 'que pasa loco'; 
+});
+
+
+Route::get('hola/{name}', function ($name) {
+    return 'que pasa amigo '.$name; 
+});
+
+Route::get('doble/{num}', function ($num = 0) {
+    
+    $varint = 0;
+    $varint = 2 * $num;
+    return 'El doble de $num es '.$varint; 
+});
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
